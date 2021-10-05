1841ae1b4bd8a4a01fb1ccb32cf58268c0f4a927
Mahmoud S. ElGammal <74462374+mahmoud-elgammal@users.noreply.github.com>
2021-10-06 00:00:09 +0200

    remove info

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -72,7 +72,7 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   // },
   {
     label: '',
-    // href: '/info',
+    href: '#',
     icon: 'More',
     hideSubNav: true,
     items: [

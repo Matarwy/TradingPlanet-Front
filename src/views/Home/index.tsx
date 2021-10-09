7250b433b9e1c374af2f8f3010c5f29ce5f48433
Mahmoud S. ElGammal <74462374+mahmoud-elgammal@users.noreply.github.com>
2021-10-09 02:35:24 +0200

    add some features

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -54,7 +54,7 @@ const Home: React.FC = () => {
         background={
           theme.isDark
             ? 'radial-gradient(103.12% 50% at 50% 50%, #21193A 0%, #191326 100%)'
-            : 'linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)'
+            : '#f2ae3312'
         }
         index={2}
         hasCurvedDivider={false}

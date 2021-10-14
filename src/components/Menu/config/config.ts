f57cb0473005f52b23f0ee10608601e075927396
elhassan elmatarwy <elhassan214@gmail.com>
2021-10-15 00:53:35 +0200

    Update config.ts

--- src/components/Menu/config/config.ts
+++ src/components/Menu/config/config.ts
@@ -8,12 +8,12 @@ const config: (t: ContextApi['t']) => ConfigMenuItemsType[] = (t) => [
   {
     label: t('Trade'),
     icon: 'Exchange',
-    href: '/swap?outputCurrency=0x83477a06f73fff92e8cf2c6b2da8efc03294e40c',
+    href: '/swap',
     showItemsOnMobile: false,
     items: [
       {
         label: t('Exchange'),
-        href: '/swap?outputCurrency=0x83477a06f73fff92e8cf2c6b2da8efc03294e40c',
+        href: '/swap',
       },
       {
         label: t('Liquidity'),
